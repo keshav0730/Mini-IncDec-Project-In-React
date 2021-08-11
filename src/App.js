c786c2b24ccca7504018fdb776d076b614a74459
keshav <kumarmishra0301@gmail.com>
2021-08-11 16:39:47 +0530

    Add Matrial Ui Icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { useState } from 'react';
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
+import Button from '@material-ui/core/Button';
 const App = () => {
     const [num,setNum]=useState(0)
      const incNum=()=>{
@@ -21,8 +24,8 @@ const App = () => {
                 <div className="center_div">
                     <h1>{ num}</h1>
                     <div className="btn_div" >
-                        <button onClick={incNum}> Increment</button>
-                        <button onClick={decNum}>Decrement</button>
+                        <Button onClick={incNum}> <AddIcon/></Button>
+                        <Button onClick={decNum}> <RemoveIcon/></Button>
                     </div>
 
                 </div>
