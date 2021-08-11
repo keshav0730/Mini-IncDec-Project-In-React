d7a255a72c174ea01e003f90c414e785417ebeab
keshav <kumarmishra0301@gmail.com>
2021-08-11 16:57:12 +0530

    Add Style in Button  Icons

--- src/App.js
+++ src/App.js
@@ -4,28 +4,32 @@ import AddIcon from '@material-ui/icons/Add';
 import RemoveIcon from '@material-ui/icons/Remove';
 import Button from '@material-ui/core/Button';
 const App = () => {
-    const [num,setNum]=useState(0)
-     const incNum=()=>{
-         setNum(num+1)
-     }
-     const decNum=()=>{
-         if(num>0){
-       setNum(num-1)
-         }
-         else{
-             setNum(0)
-             alert("Sorry!! Zero Limit Reached")
-         }
+    const [num, setNum] = useState(0)
+    const incNum = () => {
+        setNum(num + 1)
+    }
+    const decNum = () => {
+        if (num > 0) {
+            setNum(num - 1)
+        }
+        else {
+            setNum(0)
+            alert("Sorry!! Zero Limit Reached")
+        }
 
-     }
+    }
     return (
         <>
             <div className="main_div">
                 <div className="center_div">
-                    <h1>{ num}</h1>
+                    <h1>{num}</h1>
                     <div className="btn_div" >
-                        <Button onClick={incNum}> <AddIcon/></Button>
-                        <Button onClick={decNum}> <RemoveIcon/></Button>
+                        <Button onClick={incNum} className="btn_green">
+                            <AddIcon />
+                        </Button>
+                        <Button onClick={decNum} className="btn_red">
+                            <RemoveIcon />
+                        </Button>
                     </div>
 
                 </div>
