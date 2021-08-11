25ceb1bf3ecc3ecfe6b1fa90cdc2ac2c8897605c
keshav <kumarmishra0301@gmail.com>
2021-08-11 15:52:41 +0530

    Add all iCons and all functionalty in Inc And Desc

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useState } from 'react';
+const App = () => {
+    const [num,setNum]=useState(0)
+     const incNum=()=>{
+         setNum(num+1)
+     }
+     const decNum=()=>{
+         if(num>0){
+       setNum(num-1)
+         }
+         else{
+             setNum(0)
+             alert("Sorry!! Zero Limit Reached")
+         }
+
+     }
+    return (
+        <>
+            <div className="main_div">
+                <div className="center_div">
+                    <h1>{ num}</h1>
+                    <div className="btn_div" >
+                        <button onClick={incNum}> Increment</button>
+                        <button onClick={decNum}>Decrement</button>
+                    </div>
+
+                </div>
+
+            </div>
+        </>
+    )
+}
+export default App;
